8c86e3e558fb6875831c212c54f8896e003f26b3
vorobyov <you@example.com>
2025-05-29 20:29:46 +0300

    fix project

---
+++ dbt/imdb/models/orders/orders.md
@@ -0,0 +1,6 @@
+{% docs district_table%}
+
+data from [kafka](https://kafka.apache.org) <broker/topic> about district
+ETL on [Kafka Engine](https://clickhouse.com/docs/ru/engines/table-engines/integrations/kafka) ClickHouse
+
+{% enddocs %}
--- dbt/imdb/models/orders/orders_full.sql
+++ dbt/imdb/models/orders/orders_full.sql
@@ -1,10 +1,5 @@
 -- метод материализации, то есть просто вьюха, если нужна таблица - materialized='table', дальше реализуем это
-{{ config(materialized='view') }}
+{{ config(materialized='table') }}
 
-with orders_full as (
 select * from {{ source('default', 'orders') }} join {{ source('default', 'district') }} 
 on {{ source('default', 'orders') }}.OrderID = {{ source('default', 'district') }}.OrderID
-)
-
-select *
-from orders_full
